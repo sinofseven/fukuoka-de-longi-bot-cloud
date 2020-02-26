a73966c1fc8d08419aa615a5333cd04dad80d0ad
sinofseven <em.s.00001@gmail.com>
2020-02-27 02:05:39 +0900

    add sns topics for flows

--- src/button_function/src/index.py
+++ src/button_function/src/index.py
@@ -1,6 +1,6 @@
 from logger.decorator import lambda_auto_logging
 
 
-@lambda_auto_logging()
+@lambda_auto_logging('OPEN_SACK_FLOW_SNS_TOPIC_ARN', 'MAKE_COFFEE_FLOW_SNS_TOPIC_ARN')
 def handler(event, context):
     pass
