0a2191dfc596c8f3de601cf204aa856ce9f08ab4
sinofseven <em.s.00001@gmail.com>
2020-03-03 00:20:04 +0900

    implement button function

--- src/button_function/src/index.py
+++ src/button_function/src/index.py
@@ -1,6 +1,31 @@
 from logger.decorator import lambda_auto_logging
+from botocore.client import BaseClient
+import os
+import boto3
 
 
 @lambda_auto_logging('OPEN_SACK_FLOW_SNS_TOPIC_ARN', 'MAKE_COFFEE_FLOW_SNS_TOPIC_ARN')
 def handler(event, context):
-    pass
+    main(event)
+
+
+def main(event: dict, sns_client: BaseClient = boto3.client('sns')):
+    click_event = get_click_type(event)
+    topic_arn = os.environ['OPEN_SACK_FLOW_SNS_TOPIC_ARN'] if is_long_click(click_event) else os.environ['MAKE_COFFEE_FLOW_SNS_TOPIC_ARN']
+    publish(click_event, topic_arn, sns_client)
+
+
+def get_click_type(event: dict) -> str:
+    return event['deviceEvent']['buttonClicked']['clickType']
+
+
+def is_long_click(click_event: str) -> bool:
+    return click_event == 'LONG'
+
+
+def publish(payload: str, topic_arn: str, sns_client: BaseClient):
+    option = {
+        'TopicArn': topic_arn,
+        'Message': payload
+    }
+    sns_client.publish(**option)
