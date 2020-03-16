fce231571a2a5f01f269a822952c9bb696985535
sinofseven <em.s.00001@gmail.com>
2020-03-16 17:14:29 +0900

    fix open sack flow

--- src/open_sack_flow/1st_send_message/index.py
+++ src/open_sack_flow/1st_send_message/index.py
@@ -1,6 +1,13 @@
 from logger.decorator import lambda_auto_logging
+from logger.my_logger import MyLogger
+
+logger = MyLogger(__name__)
 
 
 @lambda_auto_logging()
 def handler(event, context):
+    return main(event)
+
+
+def main(event: dict) -> dict:
     return event
