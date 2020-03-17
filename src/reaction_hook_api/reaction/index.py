e1e5396762887183c3dc104124bb30639630fb62
sinofseven <em.s.00001@gmail.com>
2020-03-17 20:08:48 +0900

    fix sqs -> sns

--- src/reaction_hook_api/reaction/index.py
+++ src/reaction_hook_api/reaction/index.py
@@ -32,7 +32,7 @@ def create_result(event: dict) -> dict:
 def main(event: dict, ssm_client: BaseClient = boto3.client('ssm'), sns_client: BaseClient = boto3.client('sns')) -> None:
     target_channel_id = get_target_channel_id(ssm_client)
     if has_target_id(event, target_channel_id):
-        topic_arn = get_sqs_url()
+        topic_arn = get_sns_topic_arn()
         send_message(event, topic_arn, sns_client)
 
 
@@ -51,8 +51,8 @@ def has_target_id(event: dict, target_channel_id: str) -> bool:
     return event['body'].find(f'"channel":"{target_channel_id}"') > 0
 
 
-def get_sqs_url() -> str:
-    return os.environ['SQS_URL']
+def get_sns_topic_arn() -> str:
+    return os.environ['SNS_TOPIC_ARN']
 
 
 def send_message(event: dict, sns_topic_arn: str, sns_client: BaseClient) -> None:
