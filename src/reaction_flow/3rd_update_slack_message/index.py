1f279ff812f38f0753cc9f1e793f18b3f0d5fae8
sinofseven <em.s.00001@gmail.com>
2020-03-18 12:51:29 +0900

    implement 3rd update message of reaction flow

---
+++ src/reaction_flow/3rd_update_slack_message/index.py
@@ -0,0 +1,52 @@
+from typing import Tuple
+
+import boto3
+from botocore.client import BaseClient
+from slack import WebClient
+
+from logger.decorator import lambda_auto_logging
+from logger.my_logger import MyLogger
+
+logger = MyLogger(__name__)
+
+
+@lambda_auto_logging()
+def handler(event, context):
+    main(event)
+
+
+def main(event: dict, ssm_client: BaseClient = boto3.client("ssm")):
+    user_name, ts, amount, sack_times, _ = parse_event(event)
+    slack_config = get_slack_config(ssm_client)
+    slack_client = get_slack_client(slack_config)
+    option_update_message = create_update_message_option(user_name, ts, amount, sack_times, slack_config)
+    update_message(option_update_message, slack_client)
+
+
+def parse_event(event: dict) -> Tuple[str, str, int, int, int]:
+    data = event["responsePayload"]
+    return data["user_name"], data["ts"], data["amount"], data["sack_times"], data["user_times"]
+
+
+def get_slack_config(ssm_client: BaseClient) -> dict:
+    prefix = "/FukuokaDeLongiBot/Slack/"
+    option = {"Path": prefix, "WithDecryption": True}
+    resp = ssm_client.get_parameters_by_path(**option)
+    return {x["Name"][len(prefix) :]: x["Value"] for x in resp.get("Parameters", [])}
+
+
+def get_slack_client(slack_config: dict) -> WebClient:
+    return WebClient(token=slack_config["SlackBotToken"])
+
+
+def create_update_message_option(user_name: str, ts: str, amount: int, sack_times: int, slack_config: dict) -> dict:
+    return {
+        "channel": slack_config["Channel"],
+        "ts": ts,
+        "text": slack_config["ReactionMessage"].format(slack_config["CoffeeEmoji"] * amount, sack_times, user_name),
+    }
+
+
+def update_message(option: dict, client: WebClient) -> None:
+    resp = client.chat_update(**option)
+    logger.info("update message result", option=option, response=resp)
